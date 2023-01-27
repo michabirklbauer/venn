253bbd0ee675005d3e3419134f97d780fc0ad3ab
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-01-27 23:59:16 +0100

    move docstring

--- venn.py
+++ venn.py
@@ -11,56 +11,6 @@ from matplotlib import pyplot as plt
 from typing import List
 import warnings
 
-"""
-DESCRIPTION
-Wrapper with some defaults that I use for creating venn diagrams with the
-matplotlib-venn package (https://github.com/konstantint/matplotlib-venn).
-
-PARAMETERS:
-    set_1: set
-        first set of the venn diagram
-    set_2: set
-        second set of the venn diagram
-    set_3: set
-        [Optional]
-        if specified a three set venn diagram will be drawn, if not specified
-        the two set venn diagram of set_1 and set_2 will be drawn
-        DEFAULT: None
-    labels: List[str]
-        [Optional]
-        list of labels for the sets
-        DEFAULT: ["Set 1", "Set 2", "Set 3"]
-    colors: List[str]
-        [Optional]
-        list of valid matplotlib colors to use for the venn circles
-        DEFAULT: ["#4361EE", "#4CC9F0", "#F72585"]
-    alpha: float
-        [Optional]
-        color opacity
-        DEFAULT: 0.6
-    contour: bool
-        [Optional]
-        if contour should be drawn around venn circles
-        DEFAULT: False
-    linewidth: float
-        [Optional]
-        linewidth of the contour
-        DEFAULT: 0.5
-    title: str
-        [Optional]
-        title of the figure
-        DEFAULT: "Venn Diagram"
-    filename: str
-        [Optional]
-        if given the venn diagram will be saved at the specified location in
-        .png and .svg format with and without title
-        DEFAULT: None
-
-RETURNS:
-    plt.figure
-        matplotlib figure object of the created venn diagram
-"""
-
 def venn(set_1: set,
          set_2: set,
          set_3: set = None,
@@ -71,6 +21,56 @@ def venn(set_1: set,
          linewidth: float = 0.5,
          title: str = "Venn Diagram",
          filename: str = None) -> plt.figure:
+
+    """
+    DESCRIPTION
+    Wrapper with some defaults that I use for creating venn diagrams with the
+    matplotlib-venn package (https://github.com/konstantint/matplotlib-venn).
+
+    PARAMETERS:
+        set_1: set
+            first set of the venn diagram
+        set_2: set
+            second set of the venn diagram
+        set_3: set
+            [Optional]
+            if specified a three set venn diagram will be drawn, if not specified
+            the two set venn diagram of set_1 and set_2 will be drawn
+            DEFAULT: None
+        labels: List[str]
+            [Optional]
+            list of labels for the sets
+            DEFAULT: ["Set 1", "Set 2", "Set 3"]
+        colors: List[str]
+            [Optional]
+            list of valid matplotlib colors to use for the venn circles
+            DEFAULT: ["#4361EE", "#4CC9F0", "#F72585"]
+        alpha: float
+            [Optional]
+            color opacity
+            DEFAULT: 0.6
+        contour: bool
+            [Optional]
+            if contour should be drawn around venn circles
+            DEFAULT: False
+        linewidth: float
+            [Optional]
+            linewidth of the contour
+            DEFAULT: 0.5
+        title: str
+            [Optional]
+            title of the figure
+            DEFAULT: "Venn Diagram"
+        filename: str
+            [Optional]
+            if given the venn diagram will be saved at the specified location in
+            .png and .svg format with and without title
+            DEFAULT: None
+
+    RETURNS:
+        plt.figure
+            matplotlib figure object of the created venn diagram
+    """
 
     fig = plt.figure()
 
